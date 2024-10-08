6aa4dd7207d2186a50917573bb84d52d381e16e3
GreyF <greysforgettea@gmail.com>
2024-10-09 00:51:13 +0700

    gitignore modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,51 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите номер задания: ");
+        int taskNumber;
+
+        if (scanner.hasNextInt()) {
+            taskNumber = scanner.nextInt();
+        } else {
+            System.out.println("Некорректный ввод.");
+            return;
+        }
+
+        switch (taskNumber) {
+            case 1: {
+                System.out.print("Введите размер массива: ");
+                int size;
+
+                if (scanner.hasNextInt()) {
+                    size = scanner.nextInt();
+                } else {
+                    System.out.println("Некорректный ввод.");
+                    break;
+                }
+
+            }
+            case 2: {
+
+            }
+            case 3: {
+
+            }
+            case 4: {
+
+            }
+            case 5: {
+
+            }
+
+            default: {
+                System.out.println("Нет задания с таким номером.");
+                break;
+            }
+        }
+    } // end of the main() func;
+} // end of the Main class;
--- src/main/java/org/example/TaskOne.java
+++ src/main/java/org/example/TaskOne.java
@@ -1,4 +1,65 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class TaskOne {
+
+    public static void firstTaskArray(int n) {
+        // a) создать массив из N случ. чисел от 0 до 100
+        Integer[] arrayA = new Integer[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arrayA[i] = random.nextInt(101);
+        }
+        System.out.println("a) Массив случайных чисел:\n" + Arrays.toString(arrayA));
+
+        // b) На основе массива создайте список List
+        List<Integer> listA = new ArrayList<>(Arrays.asList(arrayA));
+        System.out.println("b) Список на основе массива:\n" + listA);
+
+        // c) Отсортируйте список по возрастанию
+        Collections.sort(listA);
+        System.out.println("c) Отсортировали по возрастанию:\n" + listA);
+
+        // d) Отсортируйте список в обратном порядке
+        listA.sort(Collections.reverseOrder());
+        System.out.println("d) Отсортировали в обратном порядке:\n" + listA);
+
+        // e) Перемешайте список
+        Collections.shuffle(listA);
+        System.out.println("e) Перемешали список:\n" + listA);
+
+        // f) Выполните циклический сдвиг на 1 элемент
+        Collections.rotate(listA, 1);
+        System.out.println("f) Сдвинули на 1 элемент:\n" + listA);
+
+        // g) Оставьте в списке только уникальные элементы
+        Set<Integer> removeDuplicates = new LinkedHashSet<>(listA);
+        listA = new ArrayList<>(removeDuplicates);
+        System.out.println("g) Оставили в списке только уникальные элементы:\n" + listA);
+
+        // h) Оставьте в списке только дублирующиеся элементы
+        List<Integer> duplicatesOnly = new ArrayList<>();
+        List<Integer> checker = new ArrayList<>();
+        for (Integer number : arrayA) {
+            if (!checker.contains(number)) {
+                checker.add(number);
+            } else {
+                duplicatesOnly.add(number);
+            }
+        }
+        System.out.println("g) Оставили в списке только дублирующиеся элементы:\n" + duplicatesOnly);
+
+        // i) Из списка получите массив
+        Integer[] arrayFromList = listA.toArray(new Integer[0]);
+
+        // j) Посчитайте количество вхождений каждого числа в массив
+        HashMap<Integer, Integer> occurances = new HashMap<>();
+        for (Integer number : arrayA) {
+            occurances.put(number, occurances.getOrDefault(number, 0) + 1);
+
+        }
+
+    }
 }
